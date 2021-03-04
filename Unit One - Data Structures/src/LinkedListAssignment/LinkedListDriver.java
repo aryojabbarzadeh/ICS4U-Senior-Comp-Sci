2fac5a4fe43716f7bd9fa52391c2ce3cf33adafc
Aryo Jabbarzadeh <ajabbarzadeh@bayviewglen.ca>
2021-03-03 23:53:53 -0500

    Final LinkedList Project

---
+++ Unit One - Data Structures/src/LinkedListAssignment/LinkedListDriver.java
@@ -0,0 +1,70 @@
+package LinkedListAssignment;
+
+import java.util.Scanner;
+
+public class LinkedListDriver {
+    public static void main(String[] args) {
+        Scanner ni = new Scanner (System.in);
+        ContactLinkedList contacts = new ContactLinkedList();
+        boolean ends = false;
+        Scanner options = new Scanner (System.in);
+        String yes = "Yes";
+        while (!ends){
+
+            
+            System.out.println("To add a contact press 1");
+            System.out.println("To display all contacts press 2");
+            System.out.println("To display a specific contact press 3");
+            System.out.println("To delete a contact press 4");
+            int option = Integer.parseInt(options.nextLine());
+
+            if (option == 1)
+                newContact(contacts);
+
+            else if (option == 2)
+                ContactLinkedList.printList(contacts);
+
+            else if (option == 3)
+                searchContact(contacts);
+
+            else if (option == 4)
+                deleteContact(contacts);
+
+            System.out.println();
+            System.out.println("If you would like to use the menu again please type Yes , and if not please type No");
+            String end = (ni.nextLine());
+            if (end.equals(yes)){
+                ends = false;
+                System.out.println();
+            }
+            else{
+                ends = true;
+            }
+        }
+        ni.close();
+        options.close();
+    }
+    private static void newContact(ContactLinkedList contacts){
+        Scanner in = new Scanner (System.in);
+        System.out.print("Please Enter the Last Name: ");
+        String LastN = (in.nextLine());
+        System.out.print("Please Enter the First Name: ");
+        String FirstN = (in.nextLine());
+        System.out.print("Please Enter the Phone Number: ");
+        String pNumber = (in.nextLine());
+        Contact f = new Contact(FirstN, LastN, pNumber);
+        contacts.add(f);
+    } 
+    private static void searchContact(ContactLinkedList contacts){ 
+        Scanner in = new Scanner (System.in); 
+        System.out.print("Please Enter the Last Name of the Contact: ");
+        String lName = (in.nextLine());
+        contacts.search(lName);
+    }
+    private static void deleteContact(ContactLinkedList contacts){
+        Scanner in = new Scanner (System.in);
+        System.out.println("Please enter the phone number of the contact you would like to delete: ");
+        String num = (in.nextLine());
+        contacts.remove(num);
+    }
+    }
