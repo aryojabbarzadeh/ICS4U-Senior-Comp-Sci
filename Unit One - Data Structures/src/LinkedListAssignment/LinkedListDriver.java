5c173dbad9514d695e2b5e9f52e7929af0d4a338
aryojabbarzadeh <77973994+aryojabbarzadeh@users.noreply.github.com>
2021-03-04 10:53:33 -0500

    Comments Added

--- Unit One - Data Structures/src/LinkedListAssignment/LinkedListDriver.java
+++ Unit One - Data Structures/src/LinkedListAssignment/LinkedListDriver.java
@@ -44,7 +44,7 @@ public class LinkedListDriver {
         ni.close();
         options.close();
     }
-    private static void newContact(ContactLinkedList contacts){
+    private static void newContact(ContactLinkedList contacts){ //adds a contact
         Scanner in = new Scanner (System.in);
         System.out.print("Please Enter the Last Name: ");
         String LastN = (in.nextLine());
@@ -55,13 +55,13 @@ public class LinkedListDriver {
         Contact f = new Contact(FirstN, LastN, pNumber); //puts the all the strings above into the contact
         contacts.add(f);
     } 
-    private static void searchContact(ContactLinkedList contacts){ 
+    private static void searchContact(ContactLinkedList contacts){ //searches for a specific contact
         Scanner in = new Scanner (System.in); 
         System.out.print("Please Enter the Last Name of the Contact: ");
         String lName = (in.nextLine());
         contacts.search(lName);
     }
-    private static void deleteContact(ContactLinkedList contacts){
+    private static void deleteContact(ContactLinkedList contacts){ //deletes a contact
         Scanner in = new Scanner (System.in);
         System.out.println("Please enter the phone number of the contact you would like to delete: ");
         String num = (in.nextLine());
