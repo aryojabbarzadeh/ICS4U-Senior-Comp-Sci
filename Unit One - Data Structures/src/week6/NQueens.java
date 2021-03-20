c9f3d4f69ed11700a7fe851a2ccc7f5bc78fb01a
Aryo Jabbarzadeh <ajabbarzadeh@bayviewglen.ca>
2021-03-19 23:39:16 -0400

    NQueens Final

--- Unit One - Data Structures/src/week6/NQueens.java
+++ Unit One - Data Structures/src/week6/NQueens.java
@@ -1,13 +1,55 @@
 package week6;
 
+import java.util.Scanner;
 import java.util.Stack;
 import week6.Queen;
 
 public class NQueens {
   public static void main(String[] args) {
-    solveNQueens(4);
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please enter the amount of Queens on the board");
+    int n = Integer.parseInt(in.nextLine());
+    printIt(solveNQueens(n), n);
+    in.close();
+  }
+/**
+ * in the method printIt I first create a board and place the Queens on th eboard by taking the Queens from the stacks and then we fill all the null spots with * and at last we print the board
+ * @param solveNQueens
+ * @param n
+ */
+  private static void printIt(Stack<Queen> solveNQueens, int n) {
+    String p;
+    String [][] board = new String [n][n];
+    for (int k = 0; k<n; k++){
+
+      Queen q = solveNQueens.peek();
+      int a = q.getRow()-1;
+      int b = q.getCol()-1;
+      board [a][b] = "Q";
+      solveNQueens.pop();
+    
+    }
+    for (int j = 0; j<n; j++){
+      for (int h = 0; h<n; h++){
+        if (board[j][h]==null)
+          board [j][h] = "*";
+
+      }
+    }
+    for (int y = 0; y<n; y++){
+      System.out.println();
+      for (int x = 0; x<n; x++){
+        p = board[y][x];
+        System.out.print(p+" ");
+      }
+    }
   }
 
+  /**
+   * In method solveNQueens we place the Queens in the stack
+   * @param n
+   * @return
+   */
   private static Stack<Queen> solveNQueens(int n) {
     Stack<Queen> goodQueens = new Stack<Queen>();
     int numFilled = 0;
@@ -18,8 +60,8 @@ public class NQueens {
     int col = 0;
     while (numFilled != n) {
       int row = numFilled + 1;
-      col++; // change this because it will not always be 1 fi we are shifting the queen t the right it will be col + 1
-      if (!conflict(new Queen(row, col), goodQueens)) {
+      col++;
+      if (!conflict(new Queen(row, col), goodQueens)&&(col<=n)) {
         goodQueens.push(new Queen(row, col));
         numFilled++;
         col = 0;
@@ -33,7 +75,12 @@ public class NQueens {
   }
   return goodQueens;
 }
-
+  /**
+   * the conflict method checks if two queens have a conflict by checking if two or more queens are above one another, beside one another, or have the same slope as each other. If the queens have a conflict the method will pop a queen and start everything again in the solvedNQueens method.
+   * @param queen
+   * @param goodQueens
+   * @return
+   */
   private static boolean conflict(Queen queen, Stack<Queen> goodQueens) {
     Stack<Queen> temp = new Stack<Queen>();
 
